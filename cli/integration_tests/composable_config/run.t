e9fd2adaa5615921648f665ff1a7129ffd8e5840
Mehul Kar <mehul.kar@vercel.com>
2023-01-17 19:37:37 -0800

    First pass at implementation

--- cli/integration_tests/composable_config/run.t
+++ cli/integration_tests/composable_config/run.t
@@ -1,31 +1,50 @@
 Setup
   $ . ${TESTDIR}/../setup.sh
-  $ . ${TESTDIR}/setup.sh $(pwd)
-  $ ${TURBO} run build --skip-infer --concurrency=1
-  \xe2\x80\xa2 Packages in scope: app-a, app-b (esc)
-  \xe2\x80\xa2 Running build in 2 packages (esc)
+  $ . ${TESTDIR}/setup.sh $(pwd)  
+# Build app-a, save output to a file so we can fish out the hash from the logs
+  $ ${TURBO} run build --skip-infer --filter=app-a > tmp.log
+  $ cat tmp.log
+  \xe2\x80\xa2 Packages in scope: app-a (esc)
+  \xe2\x80\xa2 Running build in 1 packages (esc)
   \xe2\x80\xa2 Remote caching disabled (esc)
-  app-b:build: cache miss, executing 8cddca67a0b41357
-  app-b:build: 
-  app-b:build: > build
-  app-b:build: > echo "building app-b" > lib/foo.txt && echo "building app-b" > dist/foo.txt
-  app-b:build: 
   app-a:build: cache miss, executing 9424a149145e735d
   app-a:build: 
   app-a:build: > build
   app-a:build: > echo "building app-a" > lib/foo.txt && echo "building app-a" > dist/foo.txt
   app-a:build: 
   
-   Tasks:    2 successful, 2 total
-  Cached:    0 cached, 2 total
+   Tasks:    1 successful, 1 total
+  Cached:    0 cached, 1 total
     Time:\s*[\.0-9]+m?s  (re)
   
-  $ for f in $TARGET_DIR/node_modules/.cache/turbo/*.tar.zst; do tar -tf $f; done
-  apps/app-b/.turbo/turbo-build.log
-  apps/app-b/dist/
-  apps/app-b/dist/.keep
-  apps/app-b/dist/foo.txt
+# Look in the saved logs for the hash, so we can inspect the tarball with the same name
+  $ HASH=$(cat tmp.log | grep -E "app-a:build.* executing .*" | awk '{print $5}')
+  $ tar -tf $TARGET_DIR/node_modules/.cache/turbo/$HASH.tar.zst;
   apps/app-a/.turbo/turbo-build.log
   apps/app-a/lib/
   apps/app-a/lib/.keep
   apps/app-a/lib/foo.txt
+
+# Build app-b
+  $ ${TURBO} run build --skip-infer --filter=app-b > tmp.log
+  $ cat tmp.log
+  \xe2\x80\xa2 Packages in scope: app-b (esc)
+  \xe2\x80\xa2 Running build in 1 packages (esc)
+  \xe2\x80\xa2 Remote caching disabled (esc)
+  app-b:build: cache miss, executing 8cddca67a0b41357
+  app-b:build: 
+  app-b:build: > build
+  app-b:build: > echo "building app-b" > lib/foo.txt && echo "building app-b" > dist/foo.txt
+  app-b:build: 
+  
+   Tasks:    1 successful, 1 total
+  Cached:    0 cached, 1 total
+    Time:\s*[\.0-9]+m?s  (re)
+  
+# Look in the saved logs for the hash, so we can inspect the tarball with the same name
+  $ HASH=$(cat tmp.log | grep -E "app-b:build.* executing .*" | awk '{print $5}')
+  $ tar -tf $TARGET_DIR/node_modules/.cache/turbo/$HASH.tar.zst;
+  apps/app-b/.turbo/turbo-build.log
+  apps/app-b/dist/
+  apps/app-b/dist/.keep
+  apps/app-b/dist/foo.txt
--- cli/internal/graph/graph.go
+++ cli/internal/graph/graph.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pyr-sh/dag"
 	"github.com/vercel/turbo/cli/internal/fs"
 	"github.com/vercel/turbo/cli/internal/nodes"
+	"github.com/vercel/turbo/cli/internal/turbopath"
 	"github.com/vercel/turbo/cli/internal/util"
 )
 
@@ -44,18 +45,100 @@ func (g *CompleteGraph) GetPackageTaskVisitor(ctx gocontext.Context, visitor fun
 			return fmt.Errorf("cannot find package %v for task %v", packageName, taskID)
 		}
 
-		// first check for package-tasks
-		taskDefinition, ok := g.Pipeline[fmt.Sprintf("%v", taskID)]
+		// Start a list of TaskDefinitions we've found for this TaskID
+		taskDefinitions := []fs.TaskDefinition{}
 
-		if !ok {
-			// then check for regular tasks
-			fallbackTaskDefinition, notcool := g.Pipeline[taskName]
-			// if neither, then bail
-			if !notcool {
-				return nil
+		// Start in the workspace directory
+		directory := turbopath.AbsoluteSystemPath(pkg.Dir)
+		turboJSONPath := directory.UntypedJoin("turbo.json")
+		_, err := fs.ReadTurboConfigFromPath(turboJSONPath)
+
+		// If there is no turbo.json in the workspace directory, we'll use the one in root
+		// and carry on
+		if err != nil {
+			rootTaskDefinition, _ := getTaskFromPipeline(g.Pipeline, taskID, taskName)
+			taskDefinitions = append(taskDefinitions, rootTaskDefinition)
+		} else {
+			//-------------------------
+			// For loop until we `break` manually.
+			// We will reassign `turboJSONPath` inside this loop, so that
+			// every time we iterate, we're starting from a new one.
+			for {
+				turboJSON, err := fs.ReadTurboConfigFromPath(turboJSONPath)
+				if err != nil {
+					return err
+				}
+
+				pipeline := turboJSON.Pipeline
+				taskDefinition, err := getTaskFromPipeline(pipeline, taskID, taskName)
+				if err != nil {
+					// we don't need to do anything if no taskDefinition was found in this pipeline
+				} else {
+					// Add it into the taskDefinitions
+					taskDefinitions = append(taskDefinitions, taskDefinition)
+
+					// If this turboJSON doesn't have an extends property, we can stop our for loop here.
+					if len(turboJSON.Extends) == 0 {
+						break
+					}
+
+					// TODO(mehulkar): Enable extending from more than one workspace.
+					if len(turboJSON.Extends) > 1 {
+						return fmt.Errorf(
+							"You can only extend from one workspace, %s extends from %v",
+							pkg.Name,
+							len(turboJSON.Extends),
+						)
+					}
+
+					// If there's an extends property, walk up to the next one
+					// Find the workspace it refers to, and and assign `directory` to it for the
+					// next iteration in this for loop.
+					for _, workspaceName := range turboJSON.Extends {
+						// TODO(mehulkar): Allow enabling from non-root workspaces
+						if workspaceName != util.RootPkgName {
+							return fmt.Errorf(
+								"You can only extend from the root workspace, %s extends from %s",
+								pkg.Name,
+								workspaceName,
+							)
+						}
+
+						workspace, ok := g.WorkspaceInfos[workspaceName]
+						if !ok {
+							// TODO: Should this be a hard error?
+							// A workspace was referenced that doesn't exist or we know nothing about
+							break
+						}
+
+						// Reassign these. The loop will run again with this new turbo.json now.
+						directory = turbopath.AbsoluteSystemPath(workspace.Dir)
+						turboJSONPath = directory.UntypedJoin("turbo.json")
+					}
+				}
 			}
-			// override if we need to...
-			taskDefinition = fallbackTaskDefinition
+			//-------------------------
+		}
+
+		// reverse the array, because we want to start with the end of the chain.
+		for i, j := 0, len(taskDefinitions)-1; i < j; i, j = i+1, j-1 {
+			taskDefinitions[i], taskDefinitions[j] = taskDefinitions[j], taskDefinitions[i]
+		}
+
+		// Start with an empty definition
+		mergedTaskDefinition := &fs.TaskDefinition{}
+
+		// For each of the TaskDefinitions we know of, merge them in
+		for _, taskDef := range taskDefinitions {
+			mergedTaskDefinition.Outputs = taskDef.Outputs
+			mergedTaskDefinition.Outputs = taskDef.Outputs
+			mergedTaskDefinition.ShouldCache = taskDef.ShouldCache
+			mergedTaskDefinition.EnvVarDependencies = taskDef.EnvVarDependencies
+			mergedTaskDefinition.TopologicalDependencies = taskDef.TopologicalDependencies
+			mergedTaskDefinition.TaskDependencies = taskDef.TaskDependencies
+			mergedTaskDefinition.Inputs = taskDef.Inputs
+			mergedTaskDefinition.OutputMode = taskDef.OutputMode
+			mergedTaskDefinition.Persistent = taskDef.Persistent
 		}
 
 		packageTask := &nodes.PackageTask{
@@ -63,9 +146,28 @@ func (g *CompleteGraph) GetPackageTaskVisitor(ctx gocontext.Context, visitor fun
 			Task:           taskName,
 			PackageName:    packageName,
 			Pkg:            pkg,
-			TaskDefinition: &taskDefinition,
+			TaskDefinition: mergedTaskDefinition,
 		}
 
 		return visitor(ctx, packageTask)
 	}
+}
+
+func getTaskFromPipeline(pipeline fs.Pipeline, taskID string, taskName string) (fs.TaskDefinition, error) {
+	// first check for package-tasks
+	taskDefinition, ok := pipeline[taskID]
+	if !ok {
+		// then check for regular tasks
+		fallbackTaskDefinition, notcool := pipeline[taskName]
+		// if neither, then bail
+		if !notcool {
+			// Return an empty fs.TaskDefinition
+			return fs.TaskDefinition{}, fmt.Errorf("No task defined in pipeline")
+		}
+
+		// override if we need to...
+		taskDefinition = fallbackTaskDefinition
+	}
+
+	return taskDefinition, nil
 }
